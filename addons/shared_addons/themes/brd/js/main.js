196edd73e8dbf0e0420e43ef0c0d3d69be5228f8
Andrew Schorr <afs35mm@gmail.com>
2014-01-08 23:29:19 -0500

    altering slideshow settings

--- addons/shared_addons/themes/brd/js/main.js
+++ addons/shared_addons/themes/brd/js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
     	$('#slider').slidesjs({
 	    	pagination: {
 	    		 active: false,
+	    	},
+	    	play : {
+	    		auto: true,
+	    		interval: 3000,
 	    	}
     	});
     });

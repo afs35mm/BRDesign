6ccac821044cd8fb4a7fbf85619ee37a0787f7e2
Andrew Schorr <afs35mm@gmail.com>
2014-01-08 23:21:06 -0500

    initial commit

---
+++ addons/shared_addons/themes/brd/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(function() {
+    	$('#slider').slidesjs({
+	    	pagination: {
+	    		 active: false,
+	    	}
+    	});
+    });
+});
---
+++ addons/shared_addons/themes/brd/theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Theme_Brd extends Theme
+{
+    public $name = 'BRD Design';
+    public $author = 'Andrew Schorr';
+    public $author_website = 'http://afs35mm.com';
+    public $website = 'http://brddesign.com/';
+    public $description = 'Theme for BRD Design Firm.';
+    public $version = '1.0';
+}
+ 
+ /* End of file theme.php */

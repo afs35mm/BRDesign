e96924453275c8a1f101124eee55a90e33b0e154
Andrew Schorr <afs35mm@gmail.com>
2014-01-13 22:05:38 -0500

    added all page stubs

---
+++ system/cms/logs/log-2014-01-14.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-14 03:20:42 --> Page Missing: about
+ERROR - 2014-01-14 03:59:09 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/navigation/controllers/admin.php 226
+ERROR - 2014-01-14 03:59:09 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/brddesign/system/codeigniter/core/Exceptions.php:183) /Applications/MAMP/htdocs/brddesign/system/codeigniter/libraries/Session/drivers/Session_cookie.php 701
+ERROR - 2014-01-14 04:01:15 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/navigation/controllers/admin.php 226
+ERROR - 2014-01-14 04:01:15 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/brddesign/system/codeigniter/core/Exceptions.php:183) /Applications/MAMP/htdocs/brddesign/system/codeigniter/libraries/Session/drivers/Session_cookie.php 701
+ERROR - 2014-01-14 04:02:03 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/navigation/controllers/admin.php 226
+ERROR - 2014-01-14 04:02:03 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/brddesign/system/codeigniter/core/Exceptions.php:183) /Applications/MAMP/htdocs/brddesign/system/codeigniter/libraries/Session/drivers/Session_cookie.php 701

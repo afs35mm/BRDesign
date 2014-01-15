383e4b6a143a09d9954475be472a7d80266f00f7
Andrew Schorr <afs35mm@gmail.com>
2014-01-15 00:20:55 -0500

    finished about, clients, and sticky nav

---
+++ system/cms/logs/log-2014-01-15.php
@@ -0,0 +1,6 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-15 02:01:16 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103
+ERROR - 2014-01-15 02:01:52 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103
+ERROR - 2014-01-15 02:03:02 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103
+ERROR - 2014-01-15 05:44:39 --> Page Missing: js/vendor/jquery-1.10.2.min.js

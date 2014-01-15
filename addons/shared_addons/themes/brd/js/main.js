2cd5b8eb8eddc97cbcd2388bd47e25ba90f76d72
Andrew Schorr <afs35mm@gmail.com>
2014-01-15 00:20:55 -0500

    finished about, clients, and sticky nav

--- addons/shared_addons/themes/brd/js/main.js
+++ addons/shared_addons/themes/brd/js/main.js
@@ -1,26 +1,3 @@
-
-	/**
-	* @name OLYMPUS-_initOlympusUiModules
-	* @exports OLYMPUS-_initOlympusUiModules as OLYMPUS.initUiModules
-	* @function
-	* @description Find OLYMPUS UI modules used on the site, and initialize those that exist.
-	* @param {object} [$scope] An option scope element to check for olympusui modules within.
-	*/
-	// _initOlympusUiModules = function($scope) {
-	// 	$scope = $scope || OLYMPUS.$content;
-	// 	var $olympusui = $scope.find('.olympusui-module'), limit = $olympusui.length, $module, module, i;
-	// 	for (i=0; i<limit; i++) {
-	// 		$module = $olympusui.eq(i);
-	// 		if ($.contains(document.documentElement, $module[0])) {
-	// 			module = $module.attr('data-olympusui-module');
-	// 		if (module && OLYMPUS[module] && OLYMPUS[module].initialize) {
-	// 			OLYMPUS[module].initialize($module, self.LAYOUT);
-	// 			}
-	// 		}
-	// 	}
-	// },
-
-
 var BRD = BRD || {};
 
 BRD.App = (function(){
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,30 @@ var BRD = BRD || {};
 BRD.App = (function(){
 
         var config = {
+        	$window: $(window),
+        	$nav: $('nav'),
+        	navTop: $('nav').offset().top,
+        };
 
+        var bindDomEvents = function(){
+        	config.$window.scroll(function(){
+		    	//console.log(config.$window.scrollTop());
+				if( config.$window.scrollTop() >= config.navTop ){
+					config.$nav.addClass('fixed');
+				}else{
+					config.$nav.removeClass('fixed');
+				}
+		    });
         };
 
         var init = function(){
             BRD.Carousel.init();
+            bindDomEvents();
+            console.log(config.navTop);
         };
 
         return {
-                init: init
+            init: init
         };
 
 })();

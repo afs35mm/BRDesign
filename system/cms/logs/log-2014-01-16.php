2cc29c4049b01df0b8deddb3fecae6a013a5e31d
Andrew Schorr <afs35mm@gmail.com>
2014-01-15 20:21:11 -0500

    got stuff working

---
+++ system/cms/logs/log-2014-01-16.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-16 01:10:15 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103
+ERROR - 2014-01-16 01:11:01 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103
+ERROR - 2014-01-16 01:12:30 --> Severity: Notice  --> Undefined variable: media_image /Applications/MAMP/htdocs/brddesign/addons/shared_addons/themes/brd/views/layouts/media.html 14
+ERROR - 2014-01-16 01:28:22 --> Severity: Warning  --> opendir(/Applications/MAMP/htdocs/brddesign/assets/page_types/default/test): failed to open dir: No such file or directory /Applications/MAMP/htdocs/brddesign/system/codeigniter/helpers/file_helper.php 114
+ERROR - 2014-01-16 01:28:33 --> Severity: Warning  --> opendir(/Applications/MAMP/htdocs/brddesign/assets/page_types/default/media): failed to open dir: No such file or directory /Applications/MAMP/htdocs/brddesign/system/codeigniter/helpers/file_helper.php 114
+ERROR - 2014-01-16 01:29:26 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Applications/MAMP/htdocs/brddesign/system/cms/modules/pages/controllers/admin.php 103

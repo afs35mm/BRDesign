72b7c7d8b5365a025654c04a24a3a30a685bfb79
AS, akqa <afs35mm@gmail.com>
2014-01-23 12:19:28 -0500

    finished styling blog

--- addons/shared_addons/themes/brd/views/modules/blog/posts.php
+++ addons/shared_addons/themes/brd/views/modules/blog/posts.php
@@ -1,8 +1,17 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div class="blog cf">
     <div id="posts" class="postList">
         {{ if posts }}
             {{ posts }}
                 <div class="post">
+                    {{post:month}}
                     <div class="head">
                     <img class="headerImg" src="http://s3.amazonaws.com/brd_blog_images/{{blog_post_image:filename}}" alt="">
                         <div class="date">
@@ -11,16 +20,19 @@
                         </div> 
                     </div>
                     <h3><a class="title" href="{{ url }}">{{ title }}</a></h3>
+                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
                     <div class="body preview">
                         {{ preview }}
                         <p class="readMore" ><a class="more" href="{{ url }}">Read More...</a></p>
                     </div>
                 </div>
             {{ /posts }}
-            bob: {{ pagination }}    
-            {{ else }}
-                {{ helper:lang line="blog:currently_no_posts" }}
-            {{ endif }}
+            <div id="pagination" class="cf">
+                {{ pagination }}  
+            </div>     
+        {{ else }}
+            {{ helper:lang line="blog:currently_no_posts" }}
+        {{ endif }}
     </div>
     {{ theme:partial name="blog-sidebar" }}
 </div>

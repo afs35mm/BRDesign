d249f14a5da7a4fac4313ea81f95f1e2597a3720
Andrew Schorr <afs35mm@gmail.com>
2014-01-24 00:37:11 -0500

    got mobile fly out working, ACTUALLY

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,8 @@ BRD.App = (function(){
                 $('.carousel').each(function(){
                     $(this).rwdCarousel('resize');
                 });
-                if( $(window).width() > 721 && $('#outerWrap').hasClass('mobileOpen') ){
-                    $('#outerWrap').removeClass('mobileOpen'));
+                if( $(window).width() >= 721 && $('#outerWrap').hasClass('mobileOpen') ){
+                    $('#outerWrap').removeClass('mobileOpen');
                 }
             });
             $('.navMenuBtn').on('click', showMobileMenu);

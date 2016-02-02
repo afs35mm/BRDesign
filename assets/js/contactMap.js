80ad6cd6427f53b4696583c8e22733854031bc1d
Andrew Schorr <afs35mm@gmail.com>
2016-02-01 22:25:30 -0500

    updated with new logo

--- assets/js/contactMap.js
+++ assets/js/contactMap.js
@@ -3,16 +3,16 @@ var BRD = BRD || {};
 BRD.contactMap = (function(){
 
 		var config = {
-			
+
 		};
 
 
         // var init = function($el, moduleInstance){
-        	
+
         // };
 
 		var init = function () {
-		var myLatlng = new google.maps.LatLng(40.725822, -74.004834);
+		var myLatlng = new google.maps.LatLng(40.759916, -73.991149);
 		var mapOptions = {
 			zoom: 4,
 			center: myLatlng
@@ -20,7 +20,7 @@ BRD.contactMap = (function(){
 
 		var map_canvas = document.getElementById('map_canvas');
 		var map_options = {
-			center: new google.maps.LatLng(40.72662, -74.004834),
+			center: new google.maps.LatLng(40.759916, -73.991149),
 			zoom: 16,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
@@ -28,8 +28,8 @@ BRD.contactMap = (function(){
 		var map = new google.maps.Map(map_canvas, map_options)
 
 		google.maps.event.addDomListener(window, 'load', init);
-		var contentString = '<span style="font-size: 18px; padding-bottom:15px; padding-right: 100px; padding-top:80px; font-weight:800;">BRDesign</span><br><br>233 Spring St<br>New York, NY 10013  <img style=" position:absolute; top:0px; left:0;" src="/assets/img/map_logo.jpg">';
-
+		var contentString = '<div style="width:160; height: 85px;"><img style="padding-bottom:5px; top:0px; left:0;" src="/assets/img/map_logo.jpg"><div style="">630 Ninth Avenue, Suite 502<br>New York, NY 10036</div></div>';
+		//<span style="font-size: 18px; padding-bottom:45px; padding-right: 120px; padding-top:80px; font-weight:800;">BRDesign</span>
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString
 		});
@@ -39,15 +39,13 @@ BRD.contactMap = (function(){
 			map: map,
 			title: 'Uluru (Ayers Rock)'
 		});
-		
-		
-		infowindow.open(map,marker);
-		
+
+			infowindow.open(map,marker);
 
 		}
 
         return {
                 init: init
         };
-        
+
 })();
